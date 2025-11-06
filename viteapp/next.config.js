e4db889ef911fcb71a3e26d07f968029b970fb6a
Bruno <bruno.ceccolini@gmail.com>
2025-11-06 18:14:54 +0000

    updates

--- viteapp/next.config.js
+++ viteapp/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Enable static export
   images: {
     unoptimized: true, // Disable image optimization to match Vite behavior
   },
